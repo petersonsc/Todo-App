d7c9a6da0e9a3db475d1a9bb8e2b8ea0634e8f6c
Peterson S Carvalho <petersonsc.ti@gmail.com>
2019-08-20 20:30:31 -0300

    First Commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { Todo } from './todo';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        FormsModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +21,17 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'todo-app'`, () => {
+  it(`should have a newTodo todo`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('todo-app');
-  });
+    expect(app.newTodo instanceof Todo).toBeTruthy();
+  }));
 
-  it('should render title in a h1 tag', () => {
+  it('should display "Todos" in h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to todo-app!');
-  });
+    expect(compiled.querySelector('h1').textContent).toContain('Todos');
+  }));
+
 });
